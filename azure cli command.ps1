4ed0f9ff6ac0c8843586bace3ad755bef7bf57be
hoondongkim <hoondongkim@gmail.com>
2018-01-27 13:07:31 +0900

    발표자료 업데이트

---
+++ azure cli command.ps1
@@ -0,0 +1,11 @@
+﻿az -h
+az provider register -n Microsoft.Batch
+az provider register -n Microsoft.BatchAI
+az role assignment create --scope /subscriptions/1aa15964-43e9-4fab-9be5-81abdcb9c8d1 --role "Network Contributor" --assignee 9fcb3732-5f52-4135-8c08-9d4bbaf203ea
+az batchai cluster create -h
+az group create -l eastus -n holkrazure
+### az batchai cluster create -l eastus -g holkrazure -n demoCluster -s Standard_NC6 -i UbuntuLTS --min 1 --max 1 -u demoUser -p demoPassword
+
+
+### Storage 생성
+az storage account create -l eastus -g holkrazure -n aitrainigstorageaz storage share create -n aiafs --account-name aitrainigstorageaz storage container create -n aicontainer --account-name aitrainigstorage
